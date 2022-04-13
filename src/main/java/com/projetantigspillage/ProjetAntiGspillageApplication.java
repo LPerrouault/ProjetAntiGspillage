44aeba901d91be91545df8b00913ee59e628b309
LPerrouault <perrouault.leo@gmail.com>
2022-04-13 16:28:57 +0200

    Initial commit

---
+++ src/main/java/com/projetantigspillage/ProjetAntiGspillageApplication.java
@@ -0,0 +1,13 @@
+package com.projetantigspillage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetAntiGspillageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetAntiGspillageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projetantigspillage/ProjetAntiGspillageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetantigspillage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetAntiGspillageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
